7fa46233b883bb9d36b4188c89cb9a3d251c606f
Tyler Pritchard <pritchard.tyler@gmail.com>
2021-06-26 10:21:52 -0700

    end of week 2 assignments

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -4,7 +4,6 @@ import { Card } from 'react-native-elements';
 
 class Contact extends Component {
 
-    
     static navigationOptions = {
         title: 'Contact Us'
     }
@@ -14,18 +13,16 @@ class Contact extends Component {
             <ScrollView>
                 <Card
                     title="Contact Information"
-                    wrapperStyle={{margin:20}}
-                >
+                    wrapperStyle={{margin: 20}}>
                     <Text>1 Nucamp Way</Text>
                     <Text>Seattle, WA 98001</Text>
                     <Text style={{marginBottom: 10}}>U.S.A.</Text>
                     <Text>Phone: 1-206-555-1234</Text>
                     <Text>Email: campsites@nucamp.co</Text>
-
                 </Card>
             </ScrollView>
         );
-    }
+    };
 }
 
 export default Contact;
